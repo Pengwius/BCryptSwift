ebd3253dd07041bbbb28d2cfa79876764c91133b
Tomasz F <tomekftomekf@gmail.com>
2021-07-28 00:31:34 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "BCryptSwift",
+    name: "BCrypt",
     platforms: [
         .macOS(.v10_10),
         .iOS(.v9),
@@ -20,7 +20,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "BCryptSwift",
+            name: "BCrypt",
             dependencies: [],
             path: "BCryptSwift/Classes"),
     ],
